6660121accce6ba39ae0df12571cc07071be2749
Abina Chowdhury <abina@etlimited.net>
2024-02-27 23:13:09 +0600

    create project

---
+++ src/components/TaskForm.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue';
+
+  defineProps(["name","time"]);
+
+</script>
+<!-- TaskForm.vue -->
+<template>
+    <div>
+      <input 
+      :value="name" @input="$emit('update:name',$event.target.value)"
+/>
+      <input v-model="tasks.name" label="Name"/>
+      <input v-model="tasks.time" label="Time"/>
+      
+      <button @click="submitForm">Submit</button>
+    </div>
+  </template>
+  
+
+  
